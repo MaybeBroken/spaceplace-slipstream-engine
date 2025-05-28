6295109bf59526fdf3a24bc809cf7fd1991aa3b1
MaybeBroken <davidsponseller123@gmail.com>
2025-05-28 16:22:47 -0600

    Add base programs and launcher

---
+++ main.py
@@ -0,0 +1,51 @@
+from threading import Thread as Thread
+from direct.showbase.ShowBase import ShowBase
+from direct.gui.DirectGui import *
+from panda3d.core import *
+from panda3d.core import loadPrcFileData
+import os
+import threading
+
+loadPrcFileData("", "win-size 350 150")
+loadPrcFileData("", "window-title Slipstream Launcher")
+
+
+class mainWindow(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.setBackgroundColor(0, 0, 0)
+        self.disableMouse()
+        self.accept("q", self.quit)
+        self.launch_client_button = DirectButton(
+            text="Launch Client",
+            pos=(0, 0, 0.35),
+            scale=0.6,
+            geom=None,
+            relief=DGG.FLAT,
+            command=self.launch_client,
+        )
+        self.launch_server_button = DirectButton(
+            text="Launch Server",
+            pos=(0, 0, -0.35),
+            scale=0.6,
+            geom=None,
+            relief=DGG.FLAT,
+            command=self.launch_server,
+        )
+
+    def launch_server(self):
+        print("Launching server program...")
+        threading.Thread(target=os.system, args=("python server/serverApp.py",)).start()
+
+    def launch_client(self):
+        print("Launching client program...")
+        threading.Thread(target=os.system, args=("python client/clientApp.py",)).start()
+
+    def quit(self):
+        print("Exiting main program...")
+        self.userExit()
+
+
+if __name__ == "__main__":
+    app = mainWindow()
+    app.run()
---
+++ server/serverApp.py
@@ -0,0 +1,33 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from panda3d.core import loadPrcFileData
+from screeninfo import get_monitors
+
+monitors = get_monitors()
+if monitors:
+    screen_x = monitors[0].width
+    screen_y = monitors[0].height
+else:
+    screen_x = 800
+    screen_y = 600
+
+loadPrcFileData("", f"win-size {screen_x} {screen_y}")
+loadPrcFileData("", "window-title Slipstream Server")
+loadPrcFileData("", "undecorated true")
+
+
+class serverProgram(ShowBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setBackgroundColor(0, 0, 0)
+        self.disableMouse()
+        self.accept("q", self.quit)
+
+    def quit(self):
+        print("Exiting server program...")
+        self.userExit()
+
+
+if __name__ == "__main__":
+    app = serverProgram()
+    app.run()
